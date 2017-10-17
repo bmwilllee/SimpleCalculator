b500ccd2531d308f89309176d8f72e3212cccd53
Will Lee <willlee9630@gmail.com>
2017-10-18 00:03:32 +0800

    Add files via upload

---
+++ Form1.Designer.cs
@@ -0,0 +1,295 @@
+﻿namespace exercise1
+{
+    partial class Calculator
+    {
+        /// <summary>
+        /// 必需的设计器变量。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清理所有正在使用的资源。
+        /// </summary>
+        /// <param name="disposing">如果应释放托管资源，为 true；否则为 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows 窗体设计器生成的代码
+
+        /// <summary>
+        /// 设计器支持所需的方法 - 不要修改
+        /// 使用代码编辑器修改此方法的内容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.Monitor = new System.Windows.Forms.Label();
+            this.Seven = new System.Windows.Forms.Button();
+            this.Four = new System.Windows.Forms.Button();
+            this.One = new System.Windows.Forms.Button();
+            this.Eight = new System.Windows.Forms.Button();
+            this.Five = new System.Windows.Forms.Button();
+            this.Two = new System.Windows.Forms.Button();
+            this.Nine = new System.Windows.Forms.Button();
+            this.Six = new System.Windows.Forms.Button();
+            this.Three = new System.Windows.Forms.Button();
+            this.Divide = new System.Windows.Forms.Button();
+            this.Multiple = new System.Windows.Forms.Button();
+            this.Minus = new System.Windows.Forms.Button();
+            this.Zero = new System.Windows.Forms.Button();
+            this.Point = new System.Windows.Forms.Button();
+            this.Equal = new System.Windows.Forms.Button();
+            this.Add = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // Monitor
+            // 
+            this.Monitor.AutoEllipsis = true;
+            this.Monitor.Font = new System.Drawing.Font("Calibri", 36F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Monitor.ForeColor = System.Drawing.SystemColors.ButtonFace;
+            this.Monitor.Location = new System.Drawing.Point(5, 9);
+            this.Monitor.Name = "Monitor";
+            this.Monitor.Size = new System.Drawing.Size(305, 79);
+            this.Monitor.TabIndex = 0;
+            this.Monitor.TextAlign = System.Drawing.ContentAlignment.MiddleRight;
+            // 
+            // Seven
+            // 
+            this.Seven.Font = new System.Drawing.Font("Calibri", 26.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Seven.Location = new System.Drawing.Point(10, 99);
+            this.Seven.Name = "Seven";
+            this.Seven.Size = new System.Drawing.Size(69, 49);
+            this.Seven.TabIndex = 5;
+            this.Seven.Text = "7";
+            this.Seven.UseVisualStyleBackColor = true;
+            this.Seven.Click += new System.EventHandler(this.Seven_Click);
+            // 
+            // Four
+            // 
+            this.Four.Font = new System.Drawing.Font("Calibri", 26.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Four.Location = new System.Drawing.Point(10, 154);
+            this.Four.Name = "Four";
+            this.Four.Size = new System.Drawing.Size(69, 49);
+            this.Four.TabIndex = 6;
+            this.Four.Text = "4";
+            this.Four.UseVisualStyleBackColor = true;
+            this.Four.Click += new System.EventHandler(this.Four_Click);
+            // 
+            // One
+            // 
+            this.One.Font = new System.Drawing.Font("Calibri", 26.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.One.Location = new System.Drawing.Point(10, 209);
+            this.One.Name = "One";
+            this.One.Size = new System.Drawing.Size(69, 49);
+            this.One.TabIndex = 7;
+            this.One.Text = "1";
+            this.One.UseVisualStyleBackColor = true;
+            this.One.Click += new System.EventHandler(this.One_Click);
+            // 
+            // Eight
+            // 
+            this.Eight.Font = new System.Drawing.Font("Calibri", 26.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Eight.Location = new System.Drawing.Point(85, 99);
+            this.Eight.Name = "Eight";
+            this.Eight.Size = new System.Drawing.Size(69, 49);
+            this.Eight.TabIndex = 8;
+            this.Eight.Text = "8";
+            this.Eight.UseVisualStyleBackColor = true;
+            this.Eight.Click += new System.EventHandler(this.Eight_Click);
+            // 
+            // Five
+            // 
+            this.Five.Font = new System.Drawing.Font("Calibri", 26.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Five.Location = new System.Drawing.Point(85, 154);
+            this.Five.Name = "Five";
+            this.Five.Size = new System.Drawing.Size(69, 49);
+            this.Five.TabIndex = 9;
+            this.Five.Text = "5";
+            this.Five.UseVisualStyleBackColor = true;
+            this.Five.Click += new System.EventHandler(this.Five_Click);
+            // 
+            // Two
+            // 
+            this.Two.Font = new System.Drawing.Font("Calibri", 26.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Two.Location = new System.Drawing.Point(85, 209);
+            this.Two.Name = "Two";
+            this.Two.Size = new System.Drawing.Size(69, 49);
+            this.Two.TabIndex = 10;
+            this.Two.Text = "2";
+            this.Two.UseVisualStyleBackColor = true;
+            this.Two.Click += new System.EventHandler(this.Two_Click);
+            // 
+            // Nine
+            // 
+            this.Nine.Font = new System.Drawing.Font("Calibri", 26.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Nine.Location = new System.Drawing.Point(160, 99);
+            this.Nine.Name = "Nine";
+            this.Nine.Size = new System.Drawing.Size(69, 49);
+            this.Nine.TabIndex = 11;
+            this.Nine.Text = "9";
+            this.Nine.UseVisualStyleBackColor = true;
+            this.Nine.Click += new System.EventHandler(this.Nine_Click);
+            // 
+            // Six
+            // 
+            this.Six.Font = new System.Drawing.Font("Calibri", 26.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Six.Location = new System.Drawing.Point(160, 154);
+            this.Six.Name = "Six";
+            this.Six.Size = new System.Drawing.Size(69, 49);
+            this.Six.TabIndex = 12;
+            this.Six.Text = "6";
+            this.Six.UseVisualStyleBackColor = true;
+            this.Six.Click += new System.EventHandler(this.Six_Click);
+            // 
+            // Three
+            // 
+            this.Three.Font = new System.Drawing.Font("Calibri", 26.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Three.Location = new System.Drawing.Point(160, 209);
+            this.Three.Name = "Three";
+            this.Three.Size = new System.Drawing.Size(69, 49);
+            this.Three.TabIndex = 13;
+            this.Three.Text = "3";
+            this.Three.UseVisualStyleBackColor = true;
+            this.Three.Click += new System.EventHandler(this.Three_Click);
+            // 
+            // Divide
+            // 
+            this.Divide.Font = new System.Drawing.Font("Calibri", 26.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Divide.Location = new System.Drawing.Point(235, 99);
+            this.Divide.Name = "Divide";
+            this.Divide.Size = new System.Drawing.Size(69, 49);
+            this.Divide.TabIndex = 14;
+            this.Divide.Text = "/";
+            this.Divide.UseVisualStyleBackColor = true;
+            this.Divide.Click += new System.EventHandler(this.Divide_Click);
+            // 
+            // Multiple
+            // 
+            this.Multiple.Font = new System.Drawing.Font("Calibri", 26.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Multiple.Location = new System.Drawing.Point(235, 154);
+            this.Multiple.Name = "Multiple";
+            this.Multiple.Size = new System.Drawing.Size(69, 49);
+            this.Multiple.TabIndex = 15;
+            this.Multiple.Text = "*";
+            this.Multiple.UseVisualStyleBackColor = true;
+            this.Multiple.Click += new System.EventHandler(this.Multiple_Click);
+            // 
+            // Minus
+            // 
+            this.Minus.Font = new System.Drawing.Font("Calibri", 26.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Minus.Location = new System.Drawing.Point(235, 209);
+            this.Minus.Name = "Minus";
+            this.Minus.Size = new System.Drawing.Size(69, 49);
+            this.Minus.TabIndex = 16;
+            this.Minus.Text = "-";
+            this.Minus.UseVisualStyleBackColor = true;
+            this.Minus.Click += new System.EventHandler(this.Minus_Click);
+            // 
+            // Zero
+            // 
+            this.Zero.Font = new System.Drawing.Font("Calibri", 26.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Zero.Location = new System.Drawing.Point(10, 264);
+            this.Zero.Name = "Zero";
+            this.Zero.Size = new System.Drawing.Size(69, 49);
+            this.Zero.TabIndex = 17;
+            this.Zero.Text = "0";
+            this.Zero.UseVisualStyleBackColor = true;
+            this.Zero.Click += new System.EventHandler(this.Zero_Click);
+            // 
+            // Point
+            // 
+            this.Point.Font = new System.Drawing.Font("Calibri", 26.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Point.Location = new System.Drawing.Point(85, 264);
+            this.Point.Name = "Point";
+            this.Point.Size = new System.Drawing.Size(69, 49);
+            this.Point.TabIndex = 18;
+            this.Point.Text = ".";
+            this.Point.UseVisualStyleBackColor = true;
+            this.Point.Click += new System.EventHandler(this.Point_Click);
+            // 
+            // Equal
+            // 
+            this.Equal.BackColor = System.Drawing.Color.DarkOrange;
+            this.Equal.Font = new System.Drawing.Font("Calibri", 26.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Equal.ForeColor = System.Drawing.SystemColors.ButtonFace;
+            this.Equal.Location = new System.Drawing.Point(160, 264);
+            this.Equal.Name = "Equal";
+            this.Equal.Size = new System.Drawing.Size(69, 49);
+            this.Equal.TabIndex = 19;
+            this.Equal.Text = "=";
+            this.Equal.TextAlign = System.Drawing.ContentAlignment.TopCenter;
+            this.Equal.UseVisualStyleBackColor = false;
+            this.Equal.Click += new System.EventHandler(this.Equal_Click);
+            // 
+            // Add
+            // 
+            this.Add.Font = new System.Drawing.Font("Calibri", 26.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Add.Location = new System.Drawing.Point(235, 264);
+            this.Add.Name = "Add";
+            this.Add.Size = new System.Drawing.Size(69, 49);
+            this.Add.TabIndex = 20;
+            this.Add.Text = "+";
+            this.Add.UseVisualStyleBackColor = true;
+            this.Add.Click += new System.EventHandler(this.Add_Click);
+            // 
+            // Calculator
+            // 
+            this.AllowDrop = true;
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.AutoValidate = System.Windows.Forms.AutoValidate.EnableAllowFocusChange;
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(28)))), ((int)(((byte)(28)))), ((int)(((byte)(28)))));
+            this.ClientSize = new System.Drawing.Size(314, 323);
+            this.Controls.Add(this.Add);
+            this.Controls.Add(this.Equal);
+            this.Controls.Add(this.Point);
+            this.Controls.Add(this.Zero);
+            this.Controls.Add(this.Minus);
+            this.Controls.Add(this.Multiple);
+            this.Controls.Add(this.Divide);
+            this.Controls.Add(this.Three);
+            this.Controls.Add(this.Six);
+            this.Controls.Add(this.Nine);
+            this.Controls.Add(this.Two);
+            this.Controls.Add(this.Five);
+            this.Controls.Add(this.Eight);
+            this.Controls.Add(this.One);
+            this.Controls.Add(this.Four);
+            this.Controls.Add(this.Seven);
+            this.Controls.Add(this.Monitor);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedDialog;
+            this.Name = "Calculator";
+            this.ShowIcon = false;
+            this.Text = "WILLLEE Calculator";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label Monitor;
+        private System.Windows.Forms.Button Seven;
+        private System.Windows.Forms.Button Four;
+        private System.Windows.Forms.Button One;
+        private System.Windows.Forms.Button Eight;
+        private System.Windows.Forms.Button Five;
+        private System.Windows.Forms.Button Two;
+        private System.Windows.Forms.Button Nine;
+        private System.Windows.Forms.Button Six;
+        private System.Windows.Forms.Button Three;
+        private System.Windows.Forms.Button Divide;
+        private System.Windows.Forms.Button Multiple;
+        private System.Windows.Forms.Button Minus;
+        private System.Windows.Forms.Button Zero;
+        private System.Windows.Forms.Button Point;
+        private System.Windows.Forms.Button Equal;
+        private System.Windows.Forms.Button Add;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,206 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace exercise1
+{
+    public partial class Calculator : Form
+    {
+        string num1 = ""; // initiate first value
+        string num2 = ""; // initiate second value
+        bool judge = false; // judge weither there are two values to calculate
+        int op = 0; // inititate operator
+        string result = "";
+
+        public string InputClick(string a) // This function is used to add a single value byte
+                                           // when click the certain button from 0 to 9 also "."
+        {
+           if(op == 0)
+            {
+                num1 += a;
+                return num1;
+            }
+            else
+            {
+                num2 += a;
+                judge = true;
+                return num2;
+            }
+        }
+
+        public void InputOprtator(int a) // function for press any operator button "+"/"-"/"*"/"/"
+        {
+            if(judge == true)
+            {
+                Calculate();
+                num1 = result;
+            }
+            if(result != "")
+            {
+                num1 = result;
+                result = "";
+            }
+            op = a;
+        }
+
+        public void Calculate()            // function for calculate
+        {
+            switch (op)
+            {
+                case 1:
+                    result = (Convert.ToDouble(num1) + Convert.ToDouble(num2)).ToString();
+                    break;
+                case 2:
+                    result = (Convert.ToDouble(num1) - Convert.ToDouble(num2)).ToString();
+                    break;
+                case 3:
+                    result = (Convert.ToDouble(num1) * Convert.ToDouble(num2)).ToString();
+                    break;
+                case 4:
+                    result = (Convert.ToDouble(num1) / Convert.ToDouble(num2)).ToString();
+                    break;
+            } // ok
+            num1 = "";
+            num2 = "";
+            Monitor.Text = result.ToString(); // Show the calculation reult
+            op = 0;
+        }
+
+        public Calculator()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+        }
+
+        private void One_Click(object sender, EventArgs e)  // button 1
+        {
+            Monitor.Text = InputClick("1");
+        }
+
+        private void Two_Click(object sender, EventArgs e)  // button 2
+        {
+            Monitor.Text = InputClick("2");
+        }
+
+        private void Three_Click(object sender, EventArgs e)  // button 3
+        {
+            Monitor.Text = InputClick("3");
+        }
+
+        private void Four_Click(object sender, EventArgs e)  // button 4
+        {
+            Monitor.Text = InputClick("4");
+        }
+
+        private void Five_Click(object sender, EventArgs e)  // button 5
+        {
+            Monitor.Text = InputClick("5");
+        }
+
+        private void Six_Click(object sender, EventArgs e)  // button 6
+        {
+            Monitor.Text = InputClick("6");
+        }
+
+        private void Seven_Click(object sender, EventArgs e) // button 7
+        {
+            Monitor.Text = InputClick("7");
+        }
+
+        private void Eight_Click(object sender, EventArgs e)  // button 8
+        {
+            Monitor.Text = InputClick("8");
+        }
+
+        private void Nine_Click(object sender, EventArgs e)  // button 9
+        {
+            Monitor.Text = InputClick("9");
+        }
+
+        private void Zero_Click(object sender, EventArgs e)  // button 0
+        {
+            if(Monitor.Text == "" || Monitor.Text == "0")
+            {
+                Monitor.Text = "0"; // To avoid mutiple "0" showed on the screen
+            }
+            else
+            {
+                Monitor.Text = InputClick("0");
+            }
+            if (op == 4){ // To avoid 0 as divisor
+                Monitor.Text = "";
+                MessageBox.Show("divisor cannot be ‘0’ ");
+            }
+        }
+
+        private void Point_Click(object sender, EventArgs e)  // button "."
+        {
+            if (Monitor.Text == "")
+            {
+                Monitor.Text = InputClick("0."); // if there no value in screen but press "." button, add "0." automatically
+            }
+            else
+            {
+                if(Monitor.Text.IndexOf(".") >= 1) // To limit only one point to assign to
+                {
+                    Monitor.Text = InputClick("");
+                }
+                else
+                {
+                    Monitor.Text = InputClick(".");
+                }
+            } // ok
+        }
+
+        private void Add_Click(object sender, EventArgs e)  // button "+"
+        {
+            InputOprtator(1);
+        }
+
+        private void Minus_Click(object sender, EventArgs e)  // button "-"
+        {
+            InputOprtator(2);
+        }
+
+        private void Multiple_Click(object sender, EventArgs e)  // button "*"
+        {
+            InputOprtator(3);
+        }
+
+        private void Divide_Click(object sender, EventArgs e)  // button "/"
+        {
+            InputOprtator(4);
+        }
+
+        private void Equal_Click(object sender, EventArgs e)  // button "="
+        {
+            if(judge == true)
+            {
+                Calculate();
+                judge = false;
+            }
+            else
+            {
+                if (num2 != "")
+                {
+                    Monitor.Text = num2;
+                }
+                else if (num1 != "")
+                {
+                    Monitor.Text = num1;
+                }
+                else
+                    Monitor.Text = "";
+            }
+        }
+    }
+}
